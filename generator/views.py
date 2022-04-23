f7b3d8d055cfac16a97eb575cb28a715d71d2fc6
twsomt <104034578+twsomt@users.noreply.github.com>
2022-04-23 20:49:59 +0300

    added html

--- generator/views.py
+++ generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello there friend!')
+    return render(request, 'generator/home.html', {'password':'jf4ds6hgs7d7k83f'})
 
 def eggs(request):
     return HttpResponse('<h1>Eggs are so tasty</h1>')
